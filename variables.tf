f182d61f19d5afb5823d2b9e3301f70040d59b8d
ariesyous <aries.yous@gmail.com>
2021-05-19 03:25:15 +0000

    updates

--- variables.tf
+++ variables.tf
@@ -1,17 +1,41 @@
-variable "symp_secret_key" {}
-variable "symp_access_key" {}
-variable "symphony_ip" {}
-variable "web_number" {}
+variable "web_number" {
+  type = number
+  default = "1"
+}
 #variable "web_number2" {}
-variable "web_ami" {}
-variable "web_instance_type" {}
+variable "web_ami" {
+  type = string
+  default = "ami-a4dc46db"
+}
+variable "web_instance_type" {
+  type = string
+  default = "t3.medium"
+}
 
-variable "db_password" {}
-variable "db_user" {}
+variable "db_password" {
+  type = string
+  default = "Cloud!Orchestration!"
+}
+
+variable "db_user" {
+  type = string
+  default = "dbadmin"
+}
 
 
 # Specify a sg with all egress access open for Symphony, uncomment this variable
 #variable "open_sg" {}
 
+Commenting out because Code Pipes will provide these by default
+
 variable "aws_secret_key" {}
 variable "aws_access_key" {}
+
+provider "aws" {
+
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region = "us-east-1"
+}
+
+
